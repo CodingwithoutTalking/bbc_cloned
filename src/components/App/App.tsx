a4976ba4f81b991b72dde3d1c40c19f2ef502f12
codingwithouttalking <codingwithouttalking@gmail.com>
2020-11-26 23:32:12 +0800

    Coding style setup

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,49 @@
-import React from 'react';
-import logo from '../../logo.svg';
-import './App.css';
+import React from "react";
+import logo from "../../logo.svg";
+import styled from "styled-components";
+
+const Container = styled.div`
+  text-align: center;
+
+  .App-logo {
+    height: 40vmin;
+    pointer-events: none;
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    .App-logo {
+      animation: App-logo-spin infinite 20s linear;
+    }
+  }
+
+  .App-header {
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background-color: #282c34;
+    color: white;
+    font-size: calc(10px + 2vmin);
+  }
+
+  .App-link {
+    color: #61dafb;
+  }
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
+    <Container>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +58,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Container>
   );
 }
 
